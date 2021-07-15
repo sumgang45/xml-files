37949c4f974bfd7487988f8b3f58a0f3acceeebf
Sharath Kumar <35133543+sharathwe45@users.noreply.github.com>
2021-07-15 15:13:47 +0530

    Create setup-kube-calico.sh

---
+++ setup-kube-calico.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+kubeadm init --kubernetes-version=1.21.2 --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl get nodes
+
+echo "[+] Please wait for a few seconds and check if the K8s Cluster is running successfully"
