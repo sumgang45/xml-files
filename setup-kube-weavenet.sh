36b6a7e4b458cbaae390a43147db38721f64e01a
Sharath Kumar <35133543+sharathwe45@users.noreply.github.com>
2021-07-22 12:27:35 +0530

    Create setup-kube-weavenet.sh

---
+++ setup-kube-weavenet.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+kubeadm init --kubernetes-version=1.21.2 --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl get nodes
+
+echo "[+] Please wait for a few seconds and check if the K8s Cluster is running successfully"
