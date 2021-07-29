cb9b6784c886f1ba775d8c5ba16b2f2eca352269
Sharath Kumar <35133543+sharathwe45@users.noreply.github.com>
2021-07-29 22:20:36 +0530

    Create setup-kube-default.sh

---
+++ setup-kube-default.sh
@@ -0,0 +1,13 @@
+n#!/usr/bin/env bash
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+sleep 30
+kubectl get nodes
+
+echo "[+] Please wait for a few seconds and check if the K8s Cluster is running successfully"
