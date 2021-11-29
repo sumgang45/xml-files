afc2386acaef48b7332074bc134a1b216a761336
Joshuawe45 <86956176+Joshuawe45@users.noreply.github.com>
2021-11-29 12:40:31 +0530

    Create k8-containerd.sh

---
+++ k8-containerd.sh
@@ -0,0 +1,9 @@
+kubeadm init --pod-network-cidr=10.244.0.0/16 --cri-socket=/run/containerd/containerd.sock
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+sleep 30
+kubectl get nodes
