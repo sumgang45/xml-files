18e5a2814697a11d6f126adf9b8aa5e72c1b2720
Nithin Jois <24427154+bondijois@users.noreply.github.com>
2022-07-14 14:42:00 +0300

    Update k8-containerd.sh

--- k8-containerd.sh
+++ k8-containerd.sh
@@ -2,8 +2,8 @@ kubeadm init --pod-network-cidr=10.244.0.0/16 --cri-socket=/run/containerd/conta
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/9de10c12c8266b0cfe09bc0d5c969ae28832239f/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/9de10c12c8266b0cfe09bc0d5c969ae28832239f/Documentation/k8s-manifests/kube-flannel-rbac.yml
 kubectl taint nodes --all node-role.kubernetes.io/master-
 sleep 30
 kubectl get nodes
