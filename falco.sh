d9ff6a12c504ad7f458c399fd575603bf3aa9e67
Joshuawe45 <86956176+Joshuawe45@users.noreply.github.com>
2023-03-07 10:16:47 +0530

    Update falco.sh

--- falco.sh
+++ falco.sh
@@ -8,4 +8,4 @@ sudo docker run --rm -i -t \
     -v /etc:/host/etc:ro \
     falcosecurity/falco-driver-loader:latest
 
-sudo docker run -d --rm -i -t     -e HOST_ROOT=/     --cap-add SYS_PTRACE --pid=host $(ls /dev/falco* | xargs -I {} echo --device {})     -v /var/run/docker.sock:/var/run/docker.sock     falcosecurity/falco-no-driver:lates
+sudo docker run -d --rm -i -t     -e HOST_ROOT=/     --cap-add SYS_PTRACE --pid=host $(ls /dev/falco* | xargs -I {} echo --device {})     -v /var/run/docker.sock:/var/run/docker.sock     falcosecurity/falco-no-driver:latest
