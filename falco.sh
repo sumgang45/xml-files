3add1eed93c071ab1923679197fb50b5896431b1
Joshuawe45 <86956176+Joshuawe45@users.noreply.github.com>
2023-03-07 10:15:27 +0530

    Create falco.sh

---
+++ falco.sh
@@ -0,0 +1,11 @@
+sudo docker run --rm -i -t \
+    --privileged \
+    -v /root/.falco:/root/.falco \
+    -v /proc:/host/proc:ro \
+    -v /boot:/host/boot:ro \
+    -v /lib/modules:/host/lib/modules \
+    -v /usr:/host/usr:ro \
+    -v /etc:/host/etc:ro \
+    falcosecurity/falco-driver-loader:latest
+
+sudo docker run -d --rm -i -t     -e HOST_ROOT=/     --cap-add SYS_PTRACE --pid=host $(ls /dev/falco* | xargs -I {} echo --device {})     -v /var/run/docker.sock:/var/run/docker.sock     falcosecurity/falco-no-driver:lates
