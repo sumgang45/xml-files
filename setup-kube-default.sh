8d75e4c91e9f9d128508bc28f9f3af416a39a80a
Kanumuru Rajesh <72786996+kanumuruwe45@users.noreply.github.com>
2023-11-30 20:20:40 +0530

    Updated the taint command to untaint the master

--- setup-kube-default.sh
+++ setup-kube-default.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
-kubeadm init --pod-network-cidr=10.244.0.0/16
+kubeadm init --pod-network-cidr=10.244.0.0/16 --cri-socket=/run/containerd/containerd.sock
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/9de10c12c8266b0cfe09bc0d5c969ae28832239f/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/9de10c12c8266b0cfe09bc0d5c969ae28832239f/Documentation/k8s-manifests/kube-flannel-rbac.yml
-kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/a7017961b64e6a4c381141e654220235fd1910cf/Documentation/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 sleep 30
 kubectl get nodes
-
 echo "[+] Please wait for a few seconds and check if the K8s Cluster is running successfully"
